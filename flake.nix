de3302138ce8e3a22541c061273e2009d04998d7
Emp. Justine <1224205+empjustine@users.noreply.github.com>
2024-06-03 01:44:51 -0300

    update nixpkgs

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  inputs.nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-23.11";
+  inputs.nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-24.05";
 
   outputs = {
     self,
@@ -17,31 +17,13 @@
         postBuild = ''
           mkdir -p $out/share/bash/ $out/etc/profile.d/
 
-          "${nixpkgs-stable.legacyPackages.${system}.atuin}/bin/atuin" init bash | sed \
-            -e 's|(atuin |("${nixpkgs-stable.legacyPackages.${system}.atuin}/bin/atuin" |g' \
-            -e 's| atuin | "${nixpkgs-stable.legacyPackages.${system}.atuin}/bin/atuin" |g' \
-            -e 's| bind | #### bind |g' \
-            -e 's|^bind |#### bind |g' >atuin-bash.sh
+          "${nixpkgs-stable.legacyPackages.${system}.atuin}/bin/atuin" init bash >$out/share/bash/atuin-bash.sh
 
-          cp atuin-bash.sh $out/share/bash/atuin-bash.sh
           ln -s "${nixpkgs-stable.legacyPackages.${system}.bash-preexec}/share/bash/bash-preexec.sh" $out/etc/profile.d/10-bash-preexec.sh
           ln -s $out/share/bash/atuin-bash.sh $out/etc/profile.d/20-atuin-bash.sh
         '';
       };
-      atuin-bash-ctrl-r = nixpkgs-stable.legacyPackages.${system}.buildEnv {
-        name = "atuin-bash-ctrl-r";
-        paths = [
-          self.packages.${system}.atuin-bash
-          ./atuin-bash-ctrl-r
-        ];
-      };
-      atuin-bash-up-arrow = nixpkgs-stable.legacyPackages.${system}.buildEnv {
-        name = "atuin-bash-up-arrow";
-        paths = [
-          self.packages.${system}.atuin-bash
-          ./atuin-bash-up-arrow
-        ];
-      };
+
       default = self.packages.${system}.atuin-bash;
     };
   };
